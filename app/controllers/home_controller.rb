5cd8239998c68264ea996d3f55f169088f9b8447
Tereza Kanzaki <tkanzaki@gmail.com>
2023-01-28 17:14:44 -0300

    busca equipe

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,11 @@ class HomeController < ApplicationController
     when "minha-equipe" then @ciclo.equipes.joins(:participacoes).where(participacoes: { participante_id: current_usuario.id })
     when "hierarquia" then @ciclo.equipes.where(equipes: { avaliador_id: current_usuario.id }).take.try(:hierarquia) || []
     else
-      @ciclo.equipes
+      if params[:q]
+        @ciclo.equipes.where("lower(nome) like ?", "%#{params[:q].downcase}%")
+      else
+        @ciclo.equipes
+      end
     end
 
     render "ciclos/show"
